9f299b0582e05fc83ea4b4dd3dbbf5140633abd3
yangzhenhua <751661816@qq.com>
2019-07-29 16:55:57 +0800

    updata

--- src/views/Cinema/index.vue
+++ src/views/Cinema/index.vue
@@ -1,12 +1,22 @@
 <template>
     <div>
-        Hello 
+        <Header title="喵喵影院"/>
+        Hello Cinema
+        <TabBar />
     </div>
 </template>
 
 <script>
+// 引入header组件
+import Header from '@/components/Header';
+// 引入TabBar组件
+import TabBar from '@/components/TabBar';
     export default {
-        
+        name: "Cinema",
+        components: {
+            Header,
+            TabBar
+        }
     }
 </script>
 
--- src/views/Mine/index.vue
+++ src/views/Mine/index.vue
@@ -1,12 +1,22 @@
 <template>
     <div>
-        Hello 
+        <Header title="我的"/>
+        Hello Mine
+        <TabBar />
     </div>
 </template>
 
 <script>
+// 引入header组件
+import Header from '@/components/Header';
+// 引入TabBar组件
+import TabBar from '@/components/TabBar';
     export default {
-        
+        name: "Mine",
+        components: {
+            Header,
+            TabBar
+        }
     }
 </script>
 
